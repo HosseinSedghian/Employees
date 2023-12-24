579b5d0cb61293904465e12f458490e7b492129f
Hossein Sedghian <HosseinSedghian@gmail.com>
2023-12-24 17:27:38 +0330

    Add Kubernetes Configs.

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -9,6 +9,7 @@ namespace Employees
         public static void Main(string[] args)
         {
             Thread.Sleep(20000);
+            string connectionString = string.Empty;
             var builder = WebApplication.CreateBuilder(args);
             {
                 builder.Services.AddControllers();
@@ -19,14 +20,17 @@ namespace Employees
                 string db_name = Environment.GetEnvironmentVariable("DB_NAME")!;
                 string user_id = Environment.GetEnvironmentVariable("USER_ID")!;
                 string password = Environment.GetEnvironmentVariable("PASSWORD")!;
-                string connectionString =
+                connectionString =
                     $"Data Source={db_server};Initial Catalog={db_name};User Id={user_id};Password={password};TrustServerCertificate=True";
+                
                 builder.Services.AddDbContext<EmployeeContext>(options =>
                     options.UseSqlServer(connectionString)
                 );
             }
             var app = builder.Build();
             {
+                var logger = app.Logger;
+                logger.LogInformation("\n======================\n" + connectionString + "\n======================\n");
                 app.MapControllers();
                 using (var scope = app.Services.CreateScope())
                 {
